a351c53447caf29160c8ea4f5da54b7d8bb5bebe
尉彧溟 <weiyumings@163.com>
2018-11-30 19:49:55 +0800

    666

--- BigFileSplit/Form1.Designer.cs
+++ BigFileSplit/Form1.Designer.cs
@@ -28,13 +28,107 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lentxtBox = new System.Windows.Forms.TextBox();
+            this.tixingLab = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.richTextBoxLog = new System.Windows.Forms.RichTextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(475, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(185, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "选择文件并拆分";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(195, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "拆分后单个文件大小（M）：";
+            // 
+            // lentxtBox
+            // 
+            this.lentxtBox.Location = new System.Drawing.Point(237, 26);
+            this.lentxtBox.Name = "lentxtBox";
+            this.lentxtBox.Size = new System.Drawing.Size(100, 25);
+            this.lentxtBox.TabIndex = 2;
+            // 
+            // tixingLab
+            // 
+            this.tixingLab.AutoSize = true;
+            this.tixingLab.Location = new System.Drawing.Point(36, 99);
+            this.tixingLab.Name = "tixingLab";
+            this.tixingLab.Size = new System.Drawing.Size(247, 15);
+            this.tixingLab.TabIndex = 3;
+            this.tixingLab.Text = "拆分后的数据将放在文件的同级目录";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.richTextBoxLog);
+            this.groupBox1.Location = new System.Drawing.Point(12, 205);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(891, 369);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "日志信息";
+            // 
+            // richTextBoxLog
+            // 
+            this.richTextBoxLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBoxLog.Location = new System.Drawing.Point(-3, 25);
+            this.richTextBoxLog.Name = "richTextBoxLog";
+            this.richTextBoxLog.Size = new System.Drawing.Size(891, 338);
+            this.richTextBoxLog.TabIndex = 0;
+            this.richTextBoxLog.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(915, 612);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.tixingLab);
+            this.Controls.Add(this.lentxtBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "大文件拆分";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox lentxtBox;
+        private System.Windows.Forms.Label tixingLab;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox richTextBoxLog;
     }
 }
 
--- BigFileSplit/Form1.cs
+++ BigFileSplit/Form1.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using DBTOOL.Helper;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
@@ -14,6 +16,187 @@ namespace BigFileSplit
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //string lujing = "F:\\vs2010\\Excel\\测试.xls";
+            string Openlujing = OpenFileDialog(openFileDialog);
+
+            //判断路径是否为空
+            if (Openlujing == null || Openlujing.Equals(null))
+            {
+                MessageBox.Show("没有选择文件，无法拆分");
+            }
+            else
+            {
+                outputLog("获取到的文件路径为" + Openlujing);
+                int KBlen = 0;
+                string lentxtBoxStr = lentxtBox.Text;
+                int len = Int32.Parse(lentxtBoxStr);
+
+                KBlen = 1024 * len;
+
+                fileSplit(Openlujing, KBlen, false, 0);
+            }
+         }
+
+
+        /// <summary>
+        /// 单个文件分割函数,
+        /// 可将任意文件fileIn分割为若干个子文件， 单个子文件最大为 len KB
+        /// delet标识文件分割完成后是否删除原文件, change为加密密匙
+        /// fileIn = "D:\\file.rar", 子文件名形如"D:\\file.rar@_1.split"
+        /// </summary>
+        public void fileSplit(String fileIn, int KBlen, bool delet, int change)
+        {
+            //输入文件校验
+            if (fileIn == null || !System.IO.File.Exists(fileIn))
+            {
+                MessageBox.Show("文件" + fileIn + "不存在！");
+                return;
+            }
+
+            //加密初始化
+            short sign = 1;
+            int num = 0, tmp;
+            if (change < 0) { sign = -1; change = -change; }
+
+            //取文件名和后缀, fileIn = "D:\\1.rar"
+
+            //从文件创建输入流
+            FileStream FileIn = new FileStream(fileIn, FileMode.Open);
+            byte[] data = new byte[1024];   //流读取,缓存空间
+            int len = 0, I = 1;             //记录子文件累积读取的KB大小, 分割的子文件序号
+
+            FileStream FileOut = null;      //输出流
+            int readLen = 0;                //每次实际读取的字节大小
+            while (readLen > 0 || (readLen = FileIn.Read(data, 0, data.Length)) > 0) //读取数据
+            {
+                //创建分割后的子文件，已有则覆盖，子文件"D:\\1.rar@_1.split"
+
+                
+                if (len == 0)
+                {
+                    String splitFileName = fileIn + "@_" + I++ + ".split";
+                    outputLog("即将拆分文件" + splitFileName);
+                    FileOut = new FileStream(splitFileName, FileMode.Create);
+                }
+
+                //加密逻辑,对data的首字节进行逻辑偏移加密
+                if (num == 0) num = change;
+                tmp = data[0] + sign * (num % 3 + 3);
+                if (tmp > 255) tmp -= 255;
+                else if (tmp < 0) tmp += 255;
+                data[0] = (byte)tmp;
+                num /= 3;
+
+                //输出，缓存数据写入子文件
+                FileOut.Write(data, 0, readLen);
+                FileOut.Flush();
+
+                //预读下一轮缓存数据
+                readLen = FileIn.Read(data, 0, data.Length);
+                if (++len >= KBlen || readLen == 0)     //子文件达到指定大小，或文件已读完
+                {
+                    FileOut.Close();                    //关闭当前输出流
+                    len = 0;
+                }
+            }
+
+            FileIn.Close();                             //关闭输入流
+            if (delet) System.IO.File.Delete(fileIn);   //删除源文件
+        }
+
+
+
+
+
+        /// <summary>
+        /// 根据用户选择的文件获取文件的路径。
+        /// 如果获取文件路径成功则返回路径，如果失败则返回mull
+        /// /// <param name="openFileDialog">OpenFileDialog</param>
+        /// </summary>
+        /// <returns>如果获取文件路径成功则返回路径，如果失败则返回mull</returns>
+        private static String OpenFileDialog(OpenFileDialog openFileDialog)
+        {
+            //展示提示语言
+            openFileDialog.Title = "仅支持.txt 或者.log文件";
+            //规定用户可以选择文件的类型，用后缀名控制，只支持【.xlsx】和【.xls】
+            openFileDialog.Filter = "Excel文件(*.txt;*.log)|*.txt;*.log";
+            //控制用户是否可以选择多个文件，ture为可以选择多个，false为不可以选择多个；
+            openFileDialog.Multiselect = false;
+
+            openFileDialog.ValidateNames = true;     //文件有效性验证ValidateNames，验证用户输入是否是一个有效的Windows文件名
+            openFileDialog.CheckFileExists = true;  //验证路径有效性
+            openFileDialog.CheckPathExists = true; //验证文件有效性
+
+
+            //判断用户是否点击了Ok按钮
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                //获取文件的的名字
+                String FileName = openFileDialog.FileNames[0];
+                //判断文件名是否为空
+                if (FileName == "" || FileName.Equals(null))
+                {
+                    return null;
+
+                }
+                else
+                {
+                    return FileName;
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lentxtBox.Text = "20";
+            tixingLab.Text = "拆分后的数据将放在文件的同级目录，格式为  原文件名@_序号.split";
+            tixingLab.ForeColor = Color.Red;
+        }
+
+
+
+
+        //日志记录
+        public void outputLog(String msg)
+        {
+            outputLog(msg, Color.Black, false);
+        }
+
+
+        //日志记录
+        public void outputLog(String msg, Color color, Boolean isBold)
+        {
+            //让文本框获取焦点，不过注释这行也能达到效果
+            this.richTextBoxLog.Focus();
+            //设置光标的位置到文本尾   
+            this.richTextBoxLog.Select(this.richTextBoxLog.TextLength, 0);
+            //滚动到控件光标处   
+            this.richTextBoxLog.ScrollToCaret();
+            //设置字体颜色
+            this.richTextBoxLog.SelectionColor = color;
+            if (isBold)
+            {
+                this.richTextBoxLog.SelectionFont = new Font(Font, FontStyle.Bold);
+            }
+
+
+
+            System.DateTime currentTime = new System.DateTime();
+            currentTime = System.DateTime.Now; //获取当前时间
+            msg = currentTime.ToString("yyyy-MM-dd HH:mm:ss") + "  " + msg;
+            this.richTextBoxLog.AppendText(msg);//输出到界面
+            this.richTextBoxLog.AppendText(Environment.NewLine);
+
+            //LogHelper.SetLog(msg);//写入日志文件
+
         }
     }
 }
